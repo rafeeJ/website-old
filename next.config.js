85a5d24f2247a90fea69a5429457268a6ff36f99
Rafee Jenkins <rafeejenkins@gmail.com>
2021-08-31 17:59:52 +0100

    Added first stuff

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+const withFonts = require('next-fonts');
+
+module.exports = withFonts({
+   enableSvg: true,
+   webpack(config, options) {
+     return config;
+   }
+}); 
