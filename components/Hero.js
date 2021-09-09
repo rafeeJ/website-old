cccb5c10b5ae1e4252f9bb93c55b104d47834bd3
Rafee Jenkins <rafeejenkins@gmail.com>
2021-09-09 17:14:45 +0100

    Added Kickstarter Banner

--- components/Hero.js
+++ components/Hero.js
@@ -11,12 +11,23 @@ export default function Hero() {
       <div className="flex justify-center flex-col items-center sm:flex-row" role="main">
 
         <div className="m-8 sm:w-3/12 md:w-3/12 lg:w-1/5 order-last sm:order-first" role="img">
-          <Image src={iphone}
-          //   className=" object-cover object-center rounded-md object-contain bg-white"
-          />
+          <Image src={iphone} alt={'Phone Screenshot'}/>
         </div>
 
-        <div className="sm:px-8 mt-8 px-4 flex-grow sm:mr-30 md:mr-20">
+        <div
+        onClick={() => {window.location.href = 'https://www.kickstarter.com/projects/rafeej/sewthis-by-abi'}} className="sm:px-8 mt-8 px-4 flex-grow sm:mr-30 md:mr-20">
+          <div className={"flex bg-green-300 rounded-lg justify-center p-5 items-center my-5"}>
+          <h1
+            className="text-4xl font-serif tracking-tight text-gray-900 leading-snug"
+            role="heading"
+            aria-level="1"
+          >
+            Support <span className="text-darkp">Sew</span><span className="text-lightp">This</span> on Kickstarter
+          </h1>
+          </div>
+        
+          
+          {/* <Image src={h} alt={'Kickstarter Logo'}/> */}
           <h1
             className="text-5xl font-serif tracking-tight text-gray-900 leading-snug"
             role="heading"
@@ -30,7 +41,7 @@ export default function Hero() {
           <p className="w-3/5 mt-4 text-gray-600 text-xl font-serif" aria-level="2">
             We are almost ready to launch! Get email updates of our progress and be one of the first to try SewThis!
           </p>
-          <div className="mt-2 flex" aria-level="3" role="button">
+          <div className="mt-2 flex" role="button">
             <Subscribe />
           </div>
         </div>
