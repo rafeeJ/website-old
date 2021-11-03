a64ca47acc74fa158a0e50b1fe6fec2672e034c4
Rafee Jenkins <rafeejenkins@gmail.com>
2021-11-03 12:08:33 +0000

    Layout updated

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -16,18 +16,27 @@ export default function Blog() {
     posts.docs.map((doc) => console.log(doc.data()));
   }
 
+  if (postsLoading) {
+    console.log('i am wainting')
+  }
+
   return (
     <Layout>
+      <div className="py-4">
       <text className="flex-grow font-serif tracking-tight text-darkp text-2xl sm:text-4xl">Welcome to the blog!</text>
-      <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-3">
-        {posts?.docs.map((post, index) => {
-          if (index == 0) {
-            return <BlogCard className="col-span-2 col-start-1" post={post.data()} />
-          } else {
-            return <BlogCard post={post.data()} />
+      </div>
+      <div className="pb-4">
+        <text className="font-serif tracking-tight text-lightp text-2xl sm:text-4xl col-span-2">Latest Post</text>
+        <BlogCard post={posts?.docs[0].data()} />
+      </div>
+      <div>
+        <text className="font-serif tracking-tight text-lightp text-2xl sm:text-4xl col-span-2">Older posts</text>
+        <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-2 gap-4">
+          {posts?.docs.slice(1).map((post, index) => {
+            return (<><BlogCard post={post.data()} /></>)
           }
-          // <div key={post.data().stub} style={{backgroundColor:'red'}}><h1>{post.data().title}</h1></div>
-        })}
+          )}
+        </div>
       </div>
     </Layout>
   )
