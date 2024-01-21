c785ebae55cf8494f19154c9f29d8c630165a383
Rafee Jenkins <rafee.jenkins@nandos.co.uk>
2024-01-21 19:07:18 +0000

    feat: adds links

--- src/features/homepage/intro.tsx
+++ src/features/homepage/intro.tsx
@@ -10,14 +10,14 @@ export const Intro = () => {
     <section className="mx-auto py-10 border-b border-gray-300">
       <h1 className={"font-medium pb-2"}>Hey, I&apos;m Rafee 👋</h1>
       <div>
-        <div className={"flex flex-col pb-2"}>
+        <div className={"flex flex-col pb-2 gap-0.5"}>
           <p>
             I&apos;m currently forecasting chicken sales and developing
             frontends at Nando&apos;s UK&I.
           </p>
           <p>Some of my favourite technologies at the moment are:</p>
         </div>
-        <div className={"flex flex-col md:items-start items-center gap-3 ml-2"}>
+        <div className={"flex flex-col items-start gap-3 ml-2"}>
           <Icon icon={<SiReact />} text={"React"} />
           <Icon icon={<SiTypescript />} text={"TypeScript"} />
           <Icon icon={<SiGooglecloud />} text={"GCP"} />
--- src/features/navbar.tsx
+++ src/features/navbar.tsx
@@ -2,8 +2,14 @@ import Link from "next/link";
 
 export const Navbar = () => {
   return (
-    <nav className={"mb-5"}>
+    <nav className={"mb-5 flex"}>
       <Link href={"/"}>home</Link>
+      <div className={"flex-grow"} />
+      <div className={"gap-4 flex"}>
+        <a href={"https://www.linkedin.com/in/rafeejenkins/"}>linkedin</a>
+        <a href={"https://github.com/rafeeJ"}>github</a>
+        <a href={"mailto:rafeejenkins@gmail.com"}>email</a>
+      </div>
     </nav>
   );
 };
